8d99bf0b40503f8e5db1809e1fcd89ee1b96ecd9
Tanner McMullen <94848716+ilovespectra@users.noreply.github.com>
2023-03-30 12:49:05 -0600

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import { useRouter } from 'next/router'
 import styles from './Header.module.css'
+import Image from 'next/image'
+
+import BubbleImage from '../public/Bubble.png';
 
 export default function Header() {
     const [walletAddress, setWalletAddress] = useState<string | null>(null)
@@ -32,7 +35,8 @@ export default function Header() {
     return (
         <>
             <div className="lg:max-w-6xl m-auto">
-                <header className={`flex my-4 mx-4 lg:mx-0 justify-between ${styles.header}`}>
+                <header className={`relative flex my-4 mx-4 lg:mx-10 justify-left ${styles.header}`}>
+                    <img src="/puddle.png" alt="Puddle" className="puddle" />
                     <Link href="/">
                         <h1 className={`${styles.soap} text-6xl font-phenomenaBlack h-12 leading-6 cursor-pointer`}>soap</h1>
                     </Link>
